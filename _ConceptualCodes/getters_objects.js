dd5def3daa361b845807e90a775fb3a8fb0f7134
R A H U L <86495244+RahulBRB@users.noreply.github.com>
2024-07-20 13:29:45 +0530

    Create getters_objects.js

---
+++ _ConceptualCodes/getters_objects.js
@@ -0,0 +1,12 @@
+const robot = {
+  _model: '1E78V2',
+  _energyLevel: 100,
+  
+  get energyLevel(){
+    if(typeof this._energyLevel==='number'){
+      return `My current energy level is ${this.energyLevel}`
+    }
+  }
+};
+
+
