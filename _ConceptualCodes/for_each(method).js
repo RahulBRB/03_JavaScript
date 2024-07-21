55c1d9de7544006087de7767dd7ff68ef853b34f
R A H U L <86495244+RahulBRB@users.noreply.github.com>
2024-07-21 14:01:34 +0530

    Create for_each(method).js

---
+++ _ConceptualCodes/for_each(method).js
@@ -0,0 +1,8 @@
+const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+
+// Iterate over fruits below
+fruits.forEach(function(item){
+  console.log(`I want to eat a ${item}`);
+})
+
+fruits.forEach(item=> console.log(`I want to eat a ${item}`));
