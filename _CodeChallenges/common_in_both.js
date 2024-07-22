1279f7445734ecf70ca6828fb02a53a399c7722e
R A H U L <86495244+RahulBRB@users.noreply.github.com>
2024-07-22 16:29:46 +0530

    Create common_in_both.js

---
+++ _CodeChallenges/common_in_both.js
@@ -0,0 +1,12 @@
+const justCoolStuff = (arr1, arr2) => {
+  return arr1.filter(item=>{
+    return arr2.includes(item);
+  })
+}
+
+
+const coolStuff = ['gameboys', 'skateboards', 'backwards hats', 'fruit-by-the-foot', 'pogs', 'my room', 'temporary tattoos'];
+
+const myStuff = [ 'rules', 'fruit-by-the-foot', 'wedgies', 'sweaters', 'skateboards', 'family-night', 'my room', 'braces', 'the information superhighway']; 
+
+console.log(justCoolStuff(myStuff, coolStuff))
