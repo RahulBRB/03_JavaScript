d9153ed0df4af8d4b6da738e7abe24536206c5e6
R A H U L <86495244+RahulBRB@users.noreply.github.com>
2024-07-22 16:46:09 +0530

    Create sortSpeciesByTeeth.js

---
+++ _CodeChallenges/sortSpeciesByTeeth.js
@@ -0,0 +1,18 @@
+const speciesArray = [ {speciesName:'shark', numTeeth:50}, {speciesName:'dog', numTeeth:42}, {speciesName:'alligator', numTeeth:80}, {speciesName:'human', numTeeth:32}];
+
+const sortSpeciesByTeeth = (arr) => {
+  const compareTeeth = (sp1, sp2) => {
+    return sp1.numTeeth - sp2.numTeeth;
+  };
+  return arr.sort(compareTeeth);
+};
+
+
+console.log(sortSpeciesByTeeth(speciesArray))
+
+// Should print:
+// [ { speciesName: 'human', numTeeth: 32 },
+//   { speciesName: 'dog', numTeeth: 42 },
+//   { speciesName: 'shark', numTeeth: 50 },
+//   { speciesName: 'alligator', numTeeth: 80 } ]
+
