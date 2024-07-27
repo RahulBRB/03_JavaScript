05ebfa5ece4292d221cec5d776b2c745da90f859
R A H U L <86495244+RahulBRB@users.noreply.github.com>
2024-07-27 21:28:45 +0530

    Add files via upload

---
+++ _ConceptualCodes/JavaScript 3/static_method.js
@@ -0,0 +1,45 @@
+//Static methods are called on the class, but not on instances of the class.
+class HospitalEmployee {
+    constructor(name) {
+      this._name = name;
+      this._remainingVacationDays = 20;
+    }
+    
+    get name() {
+      return this._name;
+    }
+    
+    get remainingVacationDays() {
+      return this._remainingVacationDays;
+    }
+    
+    takeVacationDays(daysOff) {
+      this._remainingVacationDays -= daysOff;
+    }
+  
+    static generatePassword(){
+      const randomNum = Math.floor(Math.random() * 10000);
+      return randomNum;
+    }
+  }
+  
+  class Nurse extends HospitalEmployee {
+    constructor(name, certifications) {
+      super(name);
+      this._certifications = certifications;
+    } 
+    
+    get certifications() {
+      return this._certifications;
+    }
+    
+    addCertification(newCertification) {
+      this.certifications.push(newCertification);
+    }
+  }
+  
+  const nurseOlynyk = new Nurse('Olynyk', ['Trauma','Pediatrics']);
+  nurseOlynyk.takeVacationDays(5);
+  console.log(nurseOlynyk.remainingVacationDays);
+  nurseOlynyk.addCertification('Genetics');
+  console.log(nurseOlynyk.certifications);
