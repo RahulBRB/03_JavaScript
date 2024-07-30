f94d6ae7ff2c53aceb0999026c070d56745d817a
R A H U L <86495244+RahulBRB@users.noreply.github.com>
2024-07-30 17:42:57 +0530

    Create setTimeout.js

---
+++ _ConceptualCodes/JavaScript 3/Async JavaScript and HTTP Requests/setTimeout.js
@@ -0,0 +1,9 @@
+console.log("This is the first line of code in app.js.");
+
+const usingSTO = () => {
+  console.log("This line of code will log to the console last.");
+};
+
+setTimeout(usingSTO, 100);
+
+console.log("This is the last line of code in app.js.");
